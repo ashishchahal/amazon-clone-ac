e95a3bc20f904ffc1ca275fec2d491b7f3f3e769
Ashish Chahal <ashish.chahal27@gmail.com>
2020-09-21 23:38:24 +0530

    Logout functionality & ui done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,16 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../StateProvider";
+import { auth } from "../firebase";
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
   return (
     <div className="header">
       <Link to="/">
@@ -24,10 +30,14 @@ function Header() {
       </dv>
 
       <div className="header__nav">
-        <Link to="/login">
-          <div className="header__navOption">
-            <span className="header__navOptionLineOne">Hello Guest</span>
-            <span className="header__navOptionLineTwo">Sign In</span>
+        <Link to={!user && "/login"}>
+          <div className="header__navOption" onClick={handleAuthentication}>
+            <span className="header__navOptionLineOne">
+              {user ? `Hello ${user?.email}` : "Hello User"}
+            </span>
+            <span className="header__navOptionLineTwo">
+              {user ? "Log out" : "Sign In"}
+            </span>
           </div>
         </Link>
 
